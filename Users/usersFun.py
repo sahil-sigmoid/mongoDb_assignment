e9052438b803235743caf503df68096c44648401
sahil-sigmoid <sahils@sigmoidanalytics.com>
2022-02-04 09:19:04 +0530

    mongodb assignment

---
+++ Users/usersFun.py
@@ -0,0 +1,9 @@
+
+from Users.loadUsersData import usersData
+
+def Users(db):
+    # for loading data into comments collection
+    # usersData(db)
+
+    collections = db['users']
+
---
+++ main.py
@@ -0,0 +1,32 @@
+
+import pymongo
+from Comments.commentFun import Comments
+from Movies.moviesFun import Movies
+from Theaters.theatersFun import Theaters
+from Users.usersFun import Users
+if __name__ == "__main__":
+    # connecting to database
+    client =pymongo.MongoClient('mongodb://localhost:27017/')
+    #creating database
+    db = client["mflix"]
+
+    print(" ******    Comments  **************")
+    Comments(db)
+    print()
+    print(" ******    Movies  **************")
+    Movies(db)
+    print()
+    print(" ******    Theatres  **************")
+    Theaters(db)
+
+    # Users(db)
+
+    collections = db['comments']
+
+
+
+
+
+
+
+
